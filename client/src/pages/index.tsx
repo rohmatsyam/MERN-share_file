cf299052dd434024c623681b951884c0a13519bd
rohmatsyam <rohmatsyam@yahoo.com>
2022-05-09 21:45:41 +0700

    "Setup frontend"

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="my-4 text-3xl font-medium">Got a file ? Share It Like Fake News</h1>
+      <div className="w-96 flex flex-col items-center bg-gray-800 shadow-xl rounded-xl justify-center">
+        {/* dropzone component */}
+        dropzone
+        {/* render file */}
+        {/* upload button */}
+      </div>
+    </div>
+  )
+}
